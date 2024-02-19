460268d8209855a188ab9d209904e05d9909d4b1
Learnedprawn <vineshparalkar@gmail.com>
2024-02-19 17:30:07 +0530

    Input Form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import InputForm from "./components/InputForm/InputForm";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-        Button
-      </button>
+      <InputForm/>
     </>
   );
 }
